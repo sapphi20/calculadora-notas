c4ccc4c29128f86d527bb78addc4e733dbbad02e
Alejandra Alarcón <alalarco@dcc.uchile.cl>
2018-11-28 15:13:26 -0300

    Funciones para calcular notas hechas

---
+++ calc.py
@@ -0,0 +1,18 @@
+def promedio(notas):
+    return sum(notas) / len(notas)
+
+
+def examen(notas):
+    prom = promedio(notas)
+    if len(notas) > 2:
+        return round((39.5 - prom * 0.6) / 0.4)
+    else:
+        return round((39.5 - prom * 0.5) / 0.5)
+
+
+def examen_segunda(notas):
+    prom = promedio(notas)
+    if len(notas) > 2:
+        return round((36.5 - prom * 0.6) / 0.4)
+    else:
+        return round((36.5 - prom * 0.5) / 0.5)
