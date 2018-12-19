00120685404e598343a4342982360e6662a431d8
Alejandra Alarcón <31349247+sapphi20@users.noreply.github.com>
2018-12-19 15:20:16 -0300

    Minor fix calc.py
    Changed mark from 39.5/36.5 to 40/37 to improve calculations and avoid approximation errors.

--- calc.py
+++ calc.py
@@ -5,14 +5,14 @@ def promedio(notas):
 def examen(notas):
     prom = promedio(notas)
     if len(notas) > 2:
-        return round((39.5 - prom * 0.6) / 0.4)
+        return round((40 - prom * 0.6) / 0.4)
     else:
-        return round((39.5 - prom * 0.5) / 0.5)
+        return round((40 - prom * 0.5) / 0.5)
 
 
 def examen_segunda(notas):
     prom = promedio(notas)
     if len(notas) > 2:
-        return round((36.5 - prom * 0.6) / 0.4)
+        return round((37 - prom * 0.6) / 0.4)
     else:
-        return round((36.5 - prom * 0.5) / 0.5)
+        return round((37 - prom * 0.5) / 0.5)
